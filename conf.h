08352f0b7101649069b49205ecd3a6cc09585f40
uobikiemukot <mxs@m2.gyao.ne.jp>
2013-10-03 21:25:53 +0900

    change conf.h

--- conf.h
+++ conf.h
@@ -2,9 +2,9 @@
 char *const exec_cmd = "bash";
 
 /* load files */
-const char *map_file = "/home/nak/haru/.skk/map.sort";
-const char *dict_file = "/home/nak/haru/.skk/dict.sort";
-const char *user_file = "/home/nak/haru/.skk/userdict";
+const char *map_file = "/path/to/map";
+const char *dict_file = "/path/to/dict";
+const char *user_file = "/path/to/userdict";
 
 /* mark */
 const char *mark_cook = "▽";

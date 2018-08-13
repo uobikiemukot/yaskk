2c816e5074c7ee5a47d9c6801f3f76c77745933e
uobikiemukot <uobikiemukot@gmail.com>
2018-08-13 18:55:56 +0900

    refactor: refactoring related to line.h changes

--- skk.h
+++ skk.h
@@ -1,63 +1,51 @@
 /* skk init/die functions */
-void skk_init(struct skk_t *skk)
+bool skk_init(struct skk_t *skk)
 {
-	skk->fd   = -1;
 	skk->mode = MODE_ASCII;
 
-	line_init(&skk->current);
-	line_init(&skk->next);
-	skk->need_flush  = false;
-
-	skk->dict.table = dict_load(skkdict_file, &skk->dict.table_size, &skk->dict.entry_count);
 	skk->select     = NULL;
 	skk->index      = 0;
 
 	skk->append.ch = '\0';
 	memset(skk->append.str, '\0', LINE_LEN_MAX);
-}
 
-void skk_die(struct skk_t *skk)
-{
-	for (int i = 0; i < skk->dict.entry_count; i++)
-		free(skk->dict.table[i].lbuf);
-	free(skk->dict.table);
+	return true;
 }
 
 /* parse functions */
-bool skk_preedit(struct skk_t *skk, uint8_t ch)
+bool skk_preedit(struct skk_t *skk, struct dict_t *dict, struct edit_t *edit, uint8_t ch)
 {
-	struct dict_entry_t *entry;
+	struct entry_t *entry;
 	char preedit_str[LINE_LEN_MAX];
 
 	/* add new character */
 	if (ch != '\0')
-		append_ucs_char(&skk->next, skk->next.cursor.insert, (uint32_t) ch);
+		append_char(edit, (uint32_t) ch);
 
 	/* there is no character in line buffer */
-	if (preedit_length(&skk->next) == 0)
+	if (preedit_len(edit) == 0)
 		return false;
 
 	/* get UTF-8 string from next linebuffer */
-	if (get_utf8_str(&skk->next, skk->next.cursor.preedit, skk->next.cursor.insert - 1, preedit_str) <= 0)
+	if (get_preedit_chars(edit, preedit_str) <= 0)
 		return false;
-	logging(DEBUG, "skk_preedit() preedit_str:%s length:%d\n", preedit_str, preedit_length(&skk->next));
+	logging(DEBUG, "skk_preedit() preedit_str:%s len:%d\n", preedit_str, preedit_len(edit));
 
-	/* search keyword from dictionary */
-	if ((entry = dict_search(&skk->dict, preedit_str, strlen(preedit_str))) != NULL) {
+	/* search word from dictionary */
+	if ((entry = dict_search(dict, preedit_str, strlen(preedit_str))) != NULL) {
 		if (VERBOSE) {
-			logging(DEBUG, "entry:%s\n", entry->keyword);
-			print_arg(&entry->candidate);
+			logging(DEBUG, "entry:%s\n", entry->word);
+			print_args(&entry->args);
 		}
 
-		if(strlen(entry->keyword) == strlen(preedit_str)) {
+		if(strlen(entry->word) == strlen(preedit_str)) {
 			logging(DEBUG, "completely matched!\n");
-			remove_chars(&skk->next, skk->next.cursor.preedit, skk->next.cursor.insert - 1);
-			append_utf8_str(&skk->next, skk->next.cursor.preedit,
-				(skk->mode == MODE_SQUARE) ? entry->candidate.argv[1]: entry->candidate.argv[0]);
+			remove_preedit_chars(edit);
+			insert_preedit_chars(edit, (skk->mode == MODE_SQUARE) ? entry->args.v[1]: entry->args.v[0]);
 
 			/* special case: double consonant (tt, kk, bb et al) -> U+3063(or U+30C3) + single consonant */
 			if (strlen(preedit_str) > 1 && preedit_str[0] == preedit_str[1] && preedit_str[0] != 'n')
-				append_ucs_char(&skk->next, skk->next.cursor.insert, preedit_str[0]);
+				append_char(edit, (uint32_t) preedit_str[0]);
 			else
 				return true;
 		} else {
@@ -69,38 +57,38 @@ bool skk_preedit(struct skk_t *skk, uint8_t ch)
 			logging(DEBUG, "unmatched! but preedit_str[0] == 'n', append another 'n'\n");
 
 			/* parse 'n' + 'n' firstly, remove single consonant  */
-			swap_ucs_char(&skk->next, skk->next.cursor.preedit + 1, 'n');
-			skk_preedit(skk, '\0');
+			replace_char(edit, preedit_cursor(edit) + 1, 'n');
+			skk_preedit(skk, dict, edit, '\0');
 
 			/* restore single consonant */
-			append_ucs_char(&skk->next, skk->next.cursor.insert, preedit_str[1]);
+			append_char(edit, preedit_str[1]);
 		} else {
 			logging(DEBUG, "unmatched! remove first preedit char\n");
-			remove_ucs_char(&skk->next, skk->next.cursor.preedit);
-			skk_preedit(skk, '\0');
+			remove_char(edit, preedit_cursor(edit));
+			skk_preedit(skk, dict, edit, '\0');
 		}
 	}
 	return false;
 }
 
-void skk_select(struct skk_t *skk, uint8_t ch)
+void skk_select(struct skk_t *skk, struct dict_t *dict, struct edit_t *edit, uint8_t ch)
 {
-	char keyword[LINE_LEN_MAX + 1];
-	struct dict_entry_t *entry;
+	char word[LINE_LEN_MAX];
+	struct entry_t *entry;
 
 	if (skk->select == NULL) {
-		/* get keyword for dict lookup */
+		/* get word for dict lookup */
 		if (skk->append.ch != '\0') /* from MODE_COOK */
-			snprintf(keyword, LINE_LEN_MAX, "%s%c", skk->restore, skk->append.ch);
+			snprintf(word, LINE_LEN_MAX, "%s%c", skk->restore, skk->append.ch);
 		else                        /* from MODE_APPEND */
-			snprintf(keyword, LINE_LEN_MAX, "%s", skk->restore);
-		logging(DEBUG, "skk_select() keyword:%s\n", keyword);
+			snprintf(word, LINE_LEN_MAX, "%s", skk->restore);
+		logging(DEBUG, "skk_select() word:%s\n", word);
 
-		if ((entry = dict_search(&skk->dict, keyword, COMPARE_ALL_STRING)) != NULL) {
-			skk->select = &entry->candidate;
+		if ((entry = dict_search(dict, word, COMPARE_ALL_STRING)) != NULL) {
+			skk->select = &entry->args;
 			if (VERBOSE) {
-				logging(DEBUG, "entry:%s\n", entry->keyword);
-				print_arg(skk->select);
+				logging(DEBUG, "entry:%s\n", entry->word);
+				print_args(skk->select);
 			}
 
 			logging(DEBUG, "candidate found!\n");
@@ -108,10 +96,10 @@ void skk_select(struct skk_t *skk, uint8_t ch)
 		}
 	} else {
 		if (ch == KEY_NEXT || ch == CTRL_N) {
-			skk->index = (skk->index + 1) % skk->select->argc;
+			skk->index = (skk->index + 1) % skk->select->len;
 		} else if (ch == KEY_PREV || ch == CTRL_P) {
 			if (skk->index <= 0)
-				skk->index = skk->select->argc - 1;
+				skk->index = skk->select->len - 1;
 			else
 				skk->index--;
 		}
@@ -119,12 +107,12 @@ void skk_select(struct skk_t *skk, uint8_t ch)
 
 	if (skk->select != NULL) {
 		/* remove cook str */
-		if (line_length(&skk->next) > 1)
-			remove_chars(&skk->next, 1, skk->next.cursor.insert - 1);
+		if (line_len(edit) > 1)
+			remove_chars(edit, 1, insert_cursor(edit) - 1);
 
-		append_utf8_str(&skk->next, 1, skk->select->argv[skk->index]);
+		insert_chars(edit, 1, skk->select->v[skk->index]);
 		if (skk->append.ch != '\0')
-			append_utf8_str(&skk->next, skk->next.cursor.insert, skk->append.str);
+			append_chars(edit, skk->append.str);
 	}
 }
 
@@ -143,13 +131,13 @@ void clear_select_status(struct skk_t *skk)
 }
 
 /* prototype */
-void mode_ascii(struct skk_t *skk, uint8_t ch);
-void mode_cursive_square(struct skk_t *skk, uint8_t ch);
-void mode_cook(struct skk_t *skk, uint8_t ch);
-void mode_select(struct skk_t *skk, uint8_t ch);
-void mode_append(struct skk_t *skk, uint8_t ch);
+void mode_ascii(struct skk_t *skk, struct dict_t *dict, struct edit_t *edit, uint8_t ch);
+void mode_cursive_square(struct skk_t *skk, struct dict_t *dict, struct edit_t *edit, uint8_t ch);
+void mode_cook(struct skk_t *skk, struct dict_t *dict, struct edit_t *edit, uint8_t ch);
+void mode_select(struct skk_t *skk, struct dict_t *dict, struct edit_t *edit, uint8_t ch);
+void mode_append(struct skk_t *skk, struct dict_t *dict, struct edit_t *edit, uint8_t ch);
 
-void (*mode_func[])(struct skk_t *skk, uint8_t ch) = {
+void (*mode_func[])(struct skk_t *skk, struct dict_t *dict, struct edit_t *edit, uint8_t ch) = {
 	[MODE_ASCII]   = mode_ascii,
 	[MODE_CURSIVE] = mode_cursive_square,
 	[MODE_SQUARE]  = mode_cursive_square,
@@ -159,8 +147,10 @@ void (*mode_func[])(struct skk_t *skk, uint8_t ch) = {
 };
 
 /* mode functions */
-void mode_ascii(struct skk_t *skk, uint8_t ch)
+void mode_ascii(struct skk_t *skk, struct dict_t *dict, struct edit_t *edit, uint8_t ch)
 {
+	(void) dict;
+
 	/* check mode change */
 	if (ch == KEY_CURSIVE) {
 		change_mode(skk, MODE_CURSIVE);
@@ -169,70 +159,69 @@ void mode_ascii(struct skk_t *skk, uint8_t ch)
 
 	/* check ctrl char */
 	if (ch == BS) {
-		send_backspace(skk->fd, 1);
+		send_backspace(edit->fd, 1);
 		return;
 	} else if (iscntrl(ch)) {
-		ewrite(skk->fd, &ch, 1);
+		ewrite(edit->fd, &ch, 1);
 		return;
 	}
 
 	/* line update */
-	append_ucs_char(&skk->next, skk->next.cursor.insert, ch);
-	skk->need_flush = true;
-	//append_preedit_char(skk, ch);
+	append_char(edit, ch);
+	edit->need_clear = true;
 }
 
-void mode_cook(struct skk_t *skk, uint8_t ch)
+void mode_cook(struct skk_t *skk, struct dict_t *dict, struct edit_t *edit, uint8_t ch)
 {
 	char restore_str[LINE_LEN_MAX];
 
 	/* check mode change */
 	if (ch == KEY_CURSIVE) {
-		remove_chars(&skk->next, skk->next.cursor.preedit, skk->next.cursor.insert - 1);
-		remove_ucs_char(&skk->next, 0);
+		remove_preedit_chars(edit);
+		remove_char(edit, 0);
 		change_mode(skk, MODE_CURSIVE);
-		skk->need_flush = true;
+		edit->need_clear = true;
 		return;
 	} else if (ch == KEY_TOGGLE) {
-		remove_chars(&skk->next, skk->next.cursor.preedit, skk->next.cursor.insert - 1);
-		toggle_cursive_square(&skk->next);
+		remove_preedit_chars(edit);
+		toggle_cursive_square(edit);
 		return;
 	} else if (ch == KEY_SELECT) {
-		remove_chars(&skk->next, skk->next.cursor.preedit, skk->next.cursor.insert - 1);
-		swap_ucs_char(&skk->next, 0, MARK_SELECT);
+		remove_preedit_chars(edit);
+		replace_char(edit, 0, MARK_SELECT);
 		change_mode(skk, MODE_SELECT);
 
 		/* store cook str */
-		get_utf8_str(&skk->next, 1, skk->next.cursor.insert - 1, restore_str);
+		get_cook_chars(edit, restore_str);
 		snprintf(skk->restore, LINE_LEN_MAX, "%s", restore_str);
 
 		/* pass through to mode_select */
-		mode_select(skk, ch);
+		mode_select(skk, dict, edit, ch);
 		return;
-	} else if (isupper(ch) && cook_length(&skk->next) > 0) {
-		remove_chars(&skk->next, skk->next.cursor.preedit, skk->next.cursor.insert - 1);
+	} else if (isupper(ch) && cook_len(edit) > 0) {
+		remove_preedit_chars(edit);
 		change_mode(skk, MODE_APPEND);
 
 		/* store cook str */
-		get_utf8_str(&skk->next, 1, skk->next.cursor.insert - 1, restore_str);
+		get_cook_chars(edit, restore_str);
 		snprintf(skk->restore, LINE_LEN_MAX, "%s", restore_str);
 
 		/* save append char/pos */
-		skk->append.pos = skk->next.cursor.insert;
+		skk->append.pos = insert_cursor(edit);
 		skk->append.ch  = tolower(ch);
 
 		/* pass through to mode_append */
-		append_ucs_char(&skk->next, skk->next.cursor.insert, MARK_APPEND);
-		mode_append(skk, skk->append.ch);
+		append_char(edit, MARK_APPEND);
+		mode_append(skk, dict, edit, skk->append.ch);
 		return;
 	}
 
 	/* check ctrl char */
 	if (ch == BS) {
-		if (preedit_length(&skk->next) > 0 || cook_length(&skk->next) > 0) {
-			remove_ucs_char(&skk->next, skk->next.cursor.insert - 1);
-		} else if (line_length(&skk->next) == 1) { /* only cook mark */
-			remove_all_chars(&skk->next);
+		if (preedit_len(edit) > 0 || cook_len(edit) > 0) {
+			remove_last_char(edit);
+		} else if (line_len(edit) == 1) { /* only cook mark */
+			remove_all(edit);
 			change_mode(skk, MODE_CURSIVE);
 		}
 		return;
@@ -242,82 +231,82 @@ void mode_cook(struct skk_t *skk, uint8_t ch)
 	}
 
 	/* line update */
-	skk_preedit(skk, ch);
+	skk_preedit(skk, dict, edit, ch);
 }
 
-void mode_cursive_square(struct skk_t *skk, uint8_t ch)
+void mode_cursive_square(struct skk_t *skk, struct dict_t *dict, struct edit_t *edit, uint8_t ch)
 {
 	/* check mode change */
-	if (get_ucs_char(&skk->next, 0) != 'z' && ch == KEY_ASCII) {
+	if (get_char(edit, 0) != 'z' && ch == KEY_ASCII) {
 		/* zl used for 'RIGHTWARDS ARROW' (U+2192), so not change mode */
-		remove_chars(&skk->next, skk->next.cursor.preedit, skk->next.cursor.insert - 1);
+		remove_preedit_chars(edit);
 		change_mode(skk, MODE_ASCII);
 		return;
 	} else if (ch == KEY_TOGGLE) {
-		remove_chars(&skk->next, skk->next.cursor.preedit, skk->next.cursor.insert - 1);
+		remove_preedit_chars(edit);
 		change_mode(skk, (skk->mode == MODE_CURSIVE) ? MODE_SQUARE: MODE_CURSIVE); /* toggle mode */
 		return;
 	} else if (isupper(ch)) {
-		remove_chars(&skk->next, skk->next.cursor.preedit, skk->next.cursor.insert - 1);
+		remove_preedit_chars(edit);
 		change_mode(skk, MODE_COOK);
 
 		/* pass through to mode_cook */
-		append_ucs_char(&skk->next, 0, MARK_COOK);
-		mode_cook(skk, tolower(ch));
+		insert_char(edit, 0, MARK_COOK);
+		mode_cook(skk, dict, edit, tolower(ch));
 		return;
 	} else if (ch == ESC || ch == CTRL_G) {
 		/* for vi user */
-		remove_chars(&skk->next, skk->next.cursor.preedit, skk->next.cursor.insert - 1);
+		remove_preedit_chars(edit);
 		change_mode(skk, MODE_ASCII);
 	}
 
 	/* check ctrl char */
 	if (ch == BS) {
-		if (preedit_length(&skk->next) <= 0)
-			send_backspace(skk->fd, 1);
+		if (preedit_len(edit) <= 0)
+			send_backspace(edit->fd, 1);
 		else
-			remove_ucs_char(&skk->next, skk->next.cursor.insert - 1);
+			remove_last_char(edit);
 		return;
-	} else if (ch == CR) {
-		if (preedit_length(&skk->next) == 0)
-			ewrite(skk->fd, &ch, 1);
 	} else if (iscntrl(ch)) {
-		//ewrite(skk->fd, &ch, 1);
 		return;
 	}
 
 	/* line update */
-	skk_preedit(skk, ch);
-	if (preedit_length(&skk->next) == 0) /* cook str exist */
-		skk->need_flush = true;
+	skk_preedit(skk, dict, edit, ch);
+	if (preedit_len(edit) == 0) /* cook str exist */
+		edit->need_clear = true;
 }
 
-void mode_select(struct skk_t *skk, uint8_t ch)
+void mode_select(struct skk_t *skk, struct dict_t *dict, struct edit_t *edit, uint8_t ch)
 {
 	/* check mode change */
 	if (ch == ESC || ch == CTRL_G) {
-		remove_all_chars(&skk->next);
-		append_ucs_char(&skk->next, 0, MARK_COOK);
-		append_utf8_str(&skk->next, 1, skk->restore);
+		remove_all(edit);
+		insert_char(edit, 0, MARK_COOK);
+		insert_chars(edit, 1, skk->restore);
 		clear_select_status(skk);
 		change_mode(skk, MODE_COOK);
 		return;
 	} else if (ch == KEY_ASCII || ch == KEY_TOGGLE || ch == KEY_CURSIVE) {
-		remove_ucs_char(&skk->next, 0);
+		remove_char(edit, 0);
+		sort_candidate(skk->select, skk->index);
 		clear_select_status(skk);
-		skk->need_flush = true;
-		change_mode(skk, (ch == KEY_ASCII) ? MODE_ASCII:
-			(ch == KEY_TOGGLE) ? MODE_SQUARE: MODE_CURSIVE);
+		edit->need_clear = true;
+		change_mode(skk,
+			(ch == KEY_ASCII) ? MODE_ASCII:
+			(ch == KEY_TOGGLE) ? MODE_SQUARE:
+			MODE_CURSIVE);
 		return;
 	} else if (isgraph(ch) && (ch != KEY_NEXT && ch != KEY_PREV)) {
-		remove_ucs_char(&skk->next, 0);
+		remove_char(edit, 0);
+		sort_candidate(skk->select, skk->index);
 		clear_select_status(skk);
-		skk->need_flush = true;
-		line_update(&skk->current, &skk->next, &skk->need_flush, skk->fd);
+		edit->need_clear = true;
+		line_update(edit);
 
 		/* pass through to mode_cursive_square */
 		change_mode(skk, MODE_CURSIVE);
-		mode_cursive_square(skk, ch);
+		mode_cursive_square(skk, dict, edit, ch);
 		return;
 	}
 
@@ -330,25 +319,25 @@ void mode_select(struct skk_t *skk, uint8_t ch)
 	}
 
 	/* line update */
-	skk_select(skk, ch);
+	skk_select(skk, dict, edit, ch);
 }
 
-void mode_append(struct skk_t *skk, uint8_t ch)
+void mode_append(struct skk_t *skk, struct dict_t *dict, struct edit_t *edit, uint8_t ch)
 {
 	char append_str[LINE_LEN_MAX];
 
 	/* check mode change */
 	if (ch == KEY_CURSIVE) {
 		change_mode(skk, MODE_CURSIVE);
-		remove_chars(&skk->next, skk->append.pos, skk->next.cursor.insert - 1);
-		remove_ucs_char(&skk->next, 0);
+		remove_chars(edit, skk->append.pos, insert_cursor(edit) - 1);
+		remove_char(edit, 0);
 		clear_select_status(skk);
-		skk->need_flush = true;
+		edit->need_clear = true;
 		return;
 	} else if (ch == ESC || ch == CTRL_G) {
 		change_mode(skk, MODE_COOK);
-		remove_chars(&skk->next, skk->append.pos, skk->next.cursor.insert - 1);
-		swap_ucs_char(&skk->next, 0, MARK_COOK);
+		remove_chars(edit, skk->append.pos, insert_cursor(edit) - 1);
+		replace_char(edit, 0, MARK_COOK);
 		clear_select_status(skk);
 		return;
 	}
@@ -360,38 +349,34 @@ void mode_append(struct skk_t *skk, uint8_t ch)
 	}
 
 	/* line update */
-	//logging(DEBUG, "mode_append()\n");
-	//line_show(&skk->next);
-	if (skk_preedit(skk, ch)) {
-		remove_chars(&skk->next, skk->next.cursor.preedit, skk->next.cursor.insert - 1);
+	if (skk_preedit(skk, dict, edit, ch)) {
+		remove_preedit_chars(edit);
 		change_mode(skk, MODE_SELECT);
-		swap_ucs_char(&skk->next, 0, MARK_SELECT);
+		replace_char(edit, 0, MARK_SELECT);
 
 		/* store append str */
-		get_utf8_str(&skk->next, skk->append.pos + 1, skk->next.cursor.insert - 1, append_str);
+		get_chars(edit, skk->append.pos + 1, insert_cursor(edit) - 1, append_str);
 		snprintf(skk->append.str, LINE_LEN_MAX, "%s", append_str);
-		//append_ucs_char(&skk->next, skk->next.cursor.insert, skk->append.ch);
 
 		/* pass through to mode_select */
 		change_mode(skk, MODE_SELECT);
-		mode_select(skk, SPACE);
+		mode_select(skk, dict, edit, SPACE);
 	}
 }
 
-void parse(struct skk_t *skk, uint8_t *buf, ssize_t size)
+void parse(struct skk_t *skk, struct dict_t *dict, struct edit_t *edit, uint8_t *buf, ssize_t size)
 {
 	ssize_t i;
 	uint8_t ch;
 
 	for (i = 0; i < size; i++) {
 		ch = buf[i];
-		logging(DEBUG, "parsing... ch:0x%.2X current mode:[%d]%s\n",
-			ch, skk->mode, mode2str[skk->mode]);
+		logging(DEBUG, "parsing... ch:0x%.2X current mode:%s\n", ch, mode2str[skk->mode]);
 
 		/* if "ch" is not ascii (0x00 - 0x7F), pass through */
 		if (ch > 0x7F) {
 			logging(DEBUG, "not ascii character(0x%.2X), pass through\n", ch);
-			ewrite(skk->fd, &ch, 1);
+			ewrite(edit->fd, &ch, 1);
 			continue;
 		}
 
@@ -399,35 +384,11 @@ void parse(struct skk_t *skk, uint8_t *buf, ssize_t size)
 
 		/* mode related process */
 		if (mode_func[skk->mode])
-			mode_func[skk->mode](skk, ch);
-
-		/*
-		do {
-			if (mode_func[skk->mode])
-				mode_func[skk->mode](skk, ch);
-		} while (skk->mode_changed);
-		*/
-
-		/*
-		if (skk->mode == MODE_ASCII)
-			mode_ascii(skk, ch);
-		if (skk->mode == MODE_COOK)
-			mode_cook(skk, ch);
-		if (skk->mode == MODE_SELECT)
-			mode_select(skk, ch);
-		if (skk->mode == MODE_APPEND)
-			mode_append(skk, ch);
-		if (skk->mode == MODE_CURSIVE || skk->mode == MODE_SQUARE)
-			mode_cursive_square(skk, ch);
-		*/
+			mode_func[skk->mode](skk, dict, edit, ch);
 	}
 
-	if (VERBOSE) {
-		logging(DEBUG, "current line\n");
-		line_show(&skk->current);
-		logging(DEBUG, "next line\n");
-		line_show(&skk->next);
-	}
+	if (VERBOSE)
+		show_edit(edit);
 
-	line_update(&skk->current, &skk->next, &skk->need_flush, skk->fd);
+	line_update(edit);
 }

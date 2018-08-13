883da3773311e269b08b73195850e720bd6886d7
uobikiemukot <uobikiemukot@gmail.com>
2018-08-13 19:00:23 +0900

    refactor: refactoring error handling

--- yaskk.c
+++ yaskk.c
@@ -8,79 +8,97 @@
 
 void sig_handler(int signo)
 {
+	/* global variable */
+	extern volatile sig_atomic_t childi_is_alive, window_resized;
+	errno = 0;
+
 	if (signo == SIGCHLD) {
-		wait(NULL);
-		LoopFlag = false;
+		if (wait(NULL) < 0)
+			logging(ERROR, "wait: %s\n", strerror(errno));
+		child_is_alive = false;
 	} else if (signo == SIGWINCH) {
-		WindowResized = true;
+		window_resized = true;
 	}
 }
 
-void set_rawmode(int fd, struct termios *save_tm)
+int set_rawmode(int fd, struct termios *current_tio)
 {
-	struct termios tm;
+	struct termios tio;
 
-	tm = *save_tm;
-	tm.c_iflag = tm.c_oflag = 0;
-	tm.c_cflag &= ~CSIZE;
-	tm.c_cflag |= CS8;
-	tm.c_lflag &= ~(ECHO | ISIG | ICANON);
-	tm.c_cc[VMIN]  = 1; /* min data size (byte) */
-	tm.c_cc[VTIME] = 0; /* time out */
-	etcsetattr(fd, TCSAFLUSH, &tm);
+	tio = *current_tio;
+	tio.c_iflag = tio.c_oflag = 0;
+	tio.c_cflag &= ~CSIZE;
+	tio.c_cflag |= CS8;
+	tio.c_lflag &= ~(ECHO | ISIG | ICANON);
+	tio.c_cc[VMIN]  = 1; /* min data size (byte) */
+	tio.c_cc[VTIME] = 0; /* time out */
+
+	return etcsetattr(fd, TCSAFLUSH, &tio);
 }
 
-void tty_init(struct termios *save_tm)
+bool tty_init(struct termios *tio)
 {
 	struct sigaction sigact;
 
 	memset(&sigact, 0, sizeof(struct sigaction));
 	sigact.sa_handler = sig_handler;
 	sigact.sa_flags   = SA_RESTART;
-	esigaction(SIGCHLD, &sigact, NULL);
-	esigaction(SIGWINCH, &sigact, NULL);
 
-	etcgetattr(STDIN_FILENO, save_tm);
-	set_rawmode(STDIN_FILENO, save_tm);
+	if (esigaction(SIGCHLD, &sigact, NULL) < 0
+		|| esigaction(SIGWINCH, &sigact, NULL) < 0)
+		return false;
+
+	if (etcgetattr(STDIN_FILENO, tio) < 0)
+		logging(WARN, "cannot get current termios information\n");
+
+	if (set_rawmode(STDIN_FILENO, tio) < 0)
+		return false;
+
+	return true;
 }
 
-void tty_die(struct termios *save_tm)
+void tty_die(struct termios *tio)
 {
 	struct sigaction sigact;
 
 	memset(&sigact, 0, sizeof(struct sigaction));
 	sigact.sa_handler = SIG_DFL;
-	sigaction(SIGCHLD, &sigact, NULL);
-	sigaction(SIGWINCH, &sigact, NULL);
+	esigaction(SIGCHLD, &sigact, NULL);
+	esigaction(SIGWINCH, &sigact, NULL);
 
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, save_tm);
-	fflush(stdout);
+	etcsetattr(STDIN_FILENO, TCSAFLUSH, tio);
+	efflush(stdout);
 }
 
-void fork_and_exec(int *master, const char *cmd, char *const argv[])
+bool fork_and_exec(int *master, const char *cmd, char *const argv[])
 {
 	pid_t pid;
 	struct winsize wsize;
 
-	if (ioctl(STDIN_FILENO, TIOCGWINSZ, &wsize) < 0)
+	if (ioctl(STDIN_FILENO, TIOCGWINSZ, &wsize) < 0) {
 		logging(ERROR, "ioctl: TIOCWINSZ failed\n");
+		return false;
+	}
 
 	pid = eforkpty(master, NULL, NULL, &wsize);
-	if (pid == 0) /* child */
+	if (pid < 0)
+		return false;
+	else if (pid == 0) /* child */
 		eexecvp(cmd, argv);
-		//eexecvp(exec_cmd, (const char *[]){exec_cmd, NULL});
+
+	return true;
 }
 
-void check_fds(fd_set *fds, int stdin, int master)
+int check_fds(fd_set *fds, int fd, int master)
 {
 	struct timeval tv;
 
 	FD_ZERO(fds);
-	FD_SET(stdin, fds);
+	FD_SET(fd, fds);
 	FD_SET(master, fds);
 	tv.tv_sec  = 0;
 	tv.tv_usec = SELECT_TIMEOUT;
-	eselect(master + 1, fds, NULL, NULL, &tv);
+	return eselect(master + 1, fds, NULL, NULL, &tv);
 }
 
 int main(int argc, char *const argv[])
@@ -90,63 +108,72 @@ int main(int argc, char *const argv[])
 	ssize_t size;
 	fd_set fds;
 	struct winsize wsize;
-	struct termios save_tm;
+	struct termios tio;
 	struct skk_t skk;
+	struct edit_t edit;
+	struct dict_t dict;
 
 	/* init */
 	setlocale(LC_ALL, "");
-	tty_init(&save_tm);
-	skk_init(&skk);
+	if (!tty_init(&tio))
+		goto tty_init_failed;
 
-	/*
-	int i;
-	struct dict_entry_t *entry;
-	for (i = 0; i < skk.dict.entry_count; i++) {
-		entry = &skk.dict.table[i];
-		if (dict_search(&skk.dict, entry->keyword, strlen(entry->keyword)) != NULL)
-			fprintf(stderr, "keyword:%s found\n", entry->keyword);
-		else
-			fprintf(stderr, "keyword:%s not found\n", entry->keyword);
-	}
-	*/
+	if (!skk_init(&skk))
+		goto skk_init_failed;
+
+	if ((dict.entry = dict_load(skkdict_file, &dict.mem_size, &dict.size)) == NULL)
+		goto dict_load_failed;
+
+	init_edit(&edit);
 
 	/* fork */
-	//argv[0] = (argc < 2) ? shell_cmd: argv[1];
 	cmd = (argc < 2) ? shell_cmd: argv[1];
-	fork_and_exec(&skk.fd, cmd, argv + 1);
+	if (!fork_and_exec(&edit.fd, cmd, argv + 1))
+		goto fork_failed;
 
 	/* main loop */
-	while (LoopFlag) {
-		check_fds(&fds, STDIN_FILENO, skk.fd);
+	while (child_is_alive) {
+		if (check_fds(&fds, STDIN_FILENO, edit.fd) == -1)
+			continue;
 
 		/* data arrived from stdin */
 		if (FD_ISSET(STDIN_FILENO, &fds)) {
 			size = read(STDIN_FILENO, buf, BUFSIZE);
 
 			if (size < INPUT_THRESHLD)
-				parse(&skk, buf, size);
+				parse(&skk, &dict, &edit, buf, size);
 			else /* large data: maybe not manual input */
-				ewrite(skk.fd, buf, size);
+				ewrite(edit.fd, buf, size);
 		}
 
 		/* data arrived from pseudo terminal */
-		if (FD_ISSET(skk.fd, &fds)) {
-			size = read(skk.fd, buf, BUFSIZE);
+		if (FD_ISSET(edit.fd, &fds)) {
+			size = read(edit.fd, buf, BUFSIZE);
 			if (size > 0)
 				ewrite(STDOUT_FILENO, buf, size);
 		}
 
 		/* if receive SIGWINCH, reach here */
-		if (WindowResized) {
-			WindowResized = false;
+		if (window_resized) {
+			window_resized = false;
 			ioctl(STDIN_FILENO, TIOCGWINSZ, &wsize);
-			ioctl(skk.fd, TIOCSWINSZ, &wsize);
+			ioctl(edit.fd, TIOCSWINSZ, &wsize);
 		}
 	}
 
 	/* normal exit */
-	skk_die(&skk);
-	tty_die(&save_tm);
+	output_dict(&dict);
+	dict_die(&dict);
+	tty_die(&tio);
 
 	return EXIT_SUCCESS;
+
+fork_failed:
+	dict_die(&dict);
+dict_load_failed:
+skk_init_failed:
+	tty_die(&tio);
+tty_init_failed:
+
+	return EXIT_FAILURE;
 }
